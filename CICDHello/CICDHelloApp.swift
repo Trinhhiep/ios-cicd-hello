9b1099c6c397a3a8f48d2a695d1e254e30ad0edb
HiepTQ <>
2026-01-05 10:30:54 +0700

    first commit

---
+++ CICDHello/CICDHelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDHelloApp.swift
+//  CICDHello
+//
+//  Created by Trinh Quang Hiep on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CICDHelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
